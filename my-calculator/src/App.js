f3cb67f2aa8ac44eaad99e23494cb9a10e9f5360
Ryan-Sherry <105294279+Ryan-Sherry@users.noreply.github.com>
2022-09-06 00:43:24 +0200

    Styling and structure complete
    Pretty much done with CSS.
    Added background image.
    Layout correct.
    6/16 tests passed.

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
 const Display = (props) => {
   return (
     <div id="display-wrapper">
-
+      <div id="screen"></div>
     </div>
   )
 }
@@ -25,7 +25,32 @@ const Display = (props) => {
 const Buttons = (props) => {
   return (
     <div id="button-wrapper">
-
+      <div id="keypad">
+        <div id="row1">
+          <button id="clear">AC</button>
+          <button id="percentage">%</button>
+          <button id="divide">/</button>
+        </div>
+        <div id="row2">
+          <button id="seven">7</button>
+          <button id="eight">8</button>
+          <button id="nine">9</button>
+          <button id="multiply">X</button>
+          <button id="four">4</button>
+          <button id="five">5</button>
+          <button id="six">6</button>
+          <button id="add">+</button>
+          <button id="one">1</button>
+          <button id="two">2</button>
+          <button id="three">3</button>
+          <button id="subtract">-</button>
+        </div>
+        <div id="row3">
+          <button id="decimal">.</button>
+          <button id="zero">0</button>
+          <button id="equals">=</button>
+        </div>
+      </div>
     </div>
   ) 
 }

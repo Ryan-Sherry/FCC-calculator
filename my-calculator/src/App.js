358d1b813f2ce9a8aff4d3d29e06c9b6a2b15e12
Ryan-Sherry <105294279+Ryan-Sherry@users.noreply.github.com>
2022-09-09 13:14:07 +0200

    Restructured entire app
    Removed individual components.
    Borrowed some code from example project (making sure I understand it first).
    % sign works, but "current" preceded by a 0.

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,67 +1,107 @@
 import React from 'react';
 import './App.css';
 
+const isOperator = /[x/+‑]/,
+  endsWithOperator = /[x+‑/]$/,
+  endsWithNegativeSign = /\d[x/+‑]{1}‑$/,
+  startsWithZero = /^0/
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentValue: 0,
-      formula: "hello"
+      current: "0",
+      prev: "",
+      formula: ""
     }
+    this.numberClick = this.numberClick.bind(this);
+    this.allClear = this.allClear.bind(this);
+    this.getAnswer = this.getAnswer.bind(this);
+    this.percentage = this.percentage.bind(this);
   }
+
+  numberClick(e) {
+    const value = e.target.value;
+    const withZero = this.state.current + value;
+
+      this.setState({
+        formula: this.state.formula + value,
+        current: withZero,
+        prev: this.state.formula
+      });
+  }
+
+  allClear() {
+    this.setState({
+      current: 0,
+      prev: 0,
+      formula: ""
+    })
+  }
+
+  percentage() {
+    let value = this.state.formula;
+    this.setState({
+      current: eval(value/100),
+      formula: value + "%" + "="
+    })
+  }
+
+  getAnswer() {
+    let expression = this.state.formula;
+    while(endsWithOperator.test(expression)) {
+      expression = expression.slice(0, -1);
+    }
+      let answer = eval(expression);
+      this.setState({
+        current: answer.toString(),
+        formula: expression + "=",
+        prev: answer
+      })
+  }
+
   render() {
     return (
       <div id='wrapper'>
         <div id="calculator">
-          <Display output={this.state.currentValue} input={this.state.formula} />
-          <Buttons />
+          <div id="display-wrapper">
+            <div id="screen">
+              <div id="output">{this.state.formula}</div>
+              <div id="display">{this.state.current}</div>
+            </div>
+          </div>
+          <div id="button-wrapper">
+            <div id="keypad">
+              <div id="row1">
+                <button onClick={this.allClear} id="clear">AC</button>
+                <button onClick={this.percentage} id="percentage">%</button>
+                <button onClick={this.operatorClick} id="divide">÷</button>
+              </div>
+              <div id="row2">
+                <button onClick={this.numberClick} id="seven" value={"7"}>7</button>
+                <button onClick={this.numberClick} id="eight" value={"8"}>8</button>
+                <button onClick={this.numberClick} id="nine" value={"9"}>9</button>
+                <button onClick={this.operatorClick} id="multiply">x</button>
+                <button onClick={this.numberClick} id="four" value={"4"}>4</button>
+                <button onClick={this.numberClick} id="five" value={"5"}>5</button>
+                <button onClick={this.numberClick} id="six" value={"6"}>6</button>
+                <button onClick={this.operatorClick} id="add">+</button>
+                <button onClick={this.numberClick} id="one" value={"1"}>1</button>
+                <button onClick={this.numberClick} id="two" value={"2"}>2</button>
+                <button onClick={this.numberClick} id="three" value={"3"}>3</button>
+                <button onClick={this.operatorClick} id="subtract">-</button>
+              </div>
+              <div id="row3">
+                <button onClick={this.handleClick} id="decimal">.</button>
+                <button onClick={this.numberClick} id="zero" value={"0"}>0</button>
+                <button onClick={this.getAnswer} id="equals">=</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-const Display = (props) => {
-  return (
-    <div id="display-wrapper">
-      <div id="display">
-        <div id="input">{props.input}</div>
-        <div id="output">{props.output}</div>
-      </div>
-    </div>
-  )
-}
-
-const Buttons = (props) => {
-  return (
-    <div id="button-wrapper">
-      <div id="keypad">
-        <div id="row1">
-          <button id="clear">AC</button>
-          <button id="percentage">%</button>
-          <button id="divide">/</button>
-        </div>
-        <div id="row2">
-          <button id="seven">7</button>
-          <button id="eight">8</button>
-          <button id="nine">9</button>
-          <button id="multiply">X</button>
-          <button id="four">4</button>
-          <button id="five">5</button>
-          <button id="six">6</button>
-          <button id="add">+</button>
-          <button id="one">1</button>
-          <button id="two">2</button>
-          <button id="three">3</button>
-          <button id="subtract">-</button>
-        </div>
-        <div id="row3">
-          <button id="decimal">.</button>
-          <button id="zero">0</button>
-          <button id="equals">=</button>
-        </div>
-      </div>
-    </div>
-  ) 
-}
 export default App;

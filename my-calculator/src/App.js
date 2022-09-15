21a19ba99343ba21fc130d2051968f070be9d4f9
Ryan-Sherry <105294279+Ryan-Sherry@users.noreply.github.com>
2022-09-15 22:47:27 +0200

    Finished reworking decimal function
    Truly believe this function now works properly.
    Added comments for each else-if statement.
    Haven't really worked on zeroClick or operatorClick.

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 
-const notZero = /[1-9]+/,
+const
   isOp = /[*+/-]/,
-  endsWithOperator = /[*+/-]$/,
-  endsWithNegativeSign = /\d[x/+‑]{1}‑$/,
   startsWithZero = /^0/,
-  startsWithZeroPoint = /^0\.\d+/,
+  endsWithOperator = /[*+/-]$/,
   endsWithDecimal = /.$/,
+  isDecNumber = /.\d*/,
+  endsWithNegativeSign = /\d[x/+‑]{1}‑$/,
+  
+  startsWithZeroPoint = /^0\.\d+/,
+  
   endsWithDecNumber = /\.(\d*)$/, 
   negs = /-{1,}$/ 
 
@@ -16,63 +19,112 @@ class App extends React.Component {
     super(props);
     this.state = {
       current: "0",
-      previous: "",
-      formula: "0",
+      formula: "",
       evaluated: false
     }
     this.numberClick = this.numberClick.bind(this);
-    this.operatorClick = this.operatorClick.bind(this);
-    this.decimalClick = this.decimalClick.bind(this);
     this.zeroClick = this.zeroClick.bind(this);
+    this.decimalClick = this.decimalClick.bind(this);
+    this.operatorClick = this.operatorClick.bind(this);
     this.allClear = this.allClear.bind(this);
-    this.getAnswer = this.getAnswer.bind(this);
     this.percentage = this.percentage.bind(this);
+    this.getAnswer = this.getAnswer.bind(this);
+    
   }
 
   numberClick(e){
     let value = e.target.value;
     const { current, formula, evaluated } = this.state;
     if(evaluated) {
+      //replaces the last answer with the "value", displaying it in "current" and "formula" fields
       this.setState({
         evaluated: false,
-        prev: current,
         current: value,
         formula: value
       })
-    } else if(endsWithDecimal.test(current) && endsWithDecimal.test(formula)){
+    } else if(isOp.test(current)){
+        //replaces the operator displayed in "current" with the typed numbers, "current" added to "formula"
         this.setState({
-          current: isOp.test(current) ? value : current + value,
+          current: value,
           formula: formula + value
         })
-    } else if(!endsWithOperator.test(current)) {
-      this.setState({
-        previous: current,
-        current: !startsWithZero.test(current) ? current + value : value,
-        formula: startsWithZero.test(formula) 
-          ? formula.slice(1,-1) + value
-          : formula + value
+    } else if(endsWithDecimal.test(current) && endsWithDecimal.test(formula)){
+        //allows numbers to be appended to decimals
+        this.setState({
+          current: current + value,
+          formula: formula + value
       })
     } else {
-      this.setState({
-        current: value,
-        formula: formula + value
-      })
+        //if cleared or just loaded, places "value" in "current" and "formula" fields
+        this.setState({
+          current: !startsWithZero.test(current) ? current + value : value,
+          formula: startsWithZero.test(formula) 
+          ? formula.slice(1,-1) + value
+          : formula + value
+        })
     }
   }
 
   zeroClick(e) {
     let value = e.target.value;
-    const { current, formula } = this.state;
-    if(endsWithOperator.test(formula)){
+    const { current, formula, evaluated } = this.state;
+    if(evaluated){
+      //allows 0 to be entered after previous answer, starting new calculation
       this.setState({
+        evaluated: false,
         current: value,
-
+        formula: value
+      })
+    } 
+    //need to sort out "ends with 0."
+    else if(endsWithOperator.test(formula)){
+        this.setState({
+          current: isOp.test(current) ? value : current,
       })
     } else {
       this.setState({
         current: startsWithZero.test(current) ? value : current + value,
         formula: startsWithZero.test(formula) ? formula.replace(/^[0]+/, "0") : formula + value
       })
+    }
+  }
+
+  decimalClick(e){
+    let value = e.target.value;
+    const { current, formula, evaluated } = this.state;
+    if(evaluated){
+      //allows decimal to be entered after previous answer, starting new calculation with "0."
+      this.setState({
+        evaluated: false,
+        current: "0" + value,
+        formula: "0" + value
+      })
+    } else if(current === "0" && formula === ""){
+      //if decimal is first button clicked, displays "0." in both fields
+      this.setState({
+        current: current + value,
+        formula: current + value
+      })
+    } else if(current.includes(".") && isDecNumber.test(formula)){
+        //prevents additional decimals after number (e.g. "1.2.1.""). Can also use "formula.endsWith(isDecNumber)"
+        this.setState({
+          current: current,
+          formula: formula
+        })
+      } else if (endsWithOperator.test(formula)){
+        //if last clicked is operator, inserts a zero before the decimal in "current" and "formula"
+        this.setState({
+          current: "0" + value,
+          formula: formula + "0" + value
+        })
+    } else {
+        //prevents more than one decimal point from being added to "current" and "formula"
+        this.setState({
+          //"if current contains a decimal, return only the current value. Otherwise return current + decimal"
+          current: current.includes(".") ? current : current + value,
+          //"if formula does not end with a decimal, return formula + decimal. Otherwise, return only formula"
+          formula: !formula.endsWith(".") ? formula + value : formula
+        })
     }
   }
 
@@ -85,68 +137,42 @@ class App extends React.Component {
     })
   }
 
-  decimalClick(e){
-    let value = e.target.value;
-    const { current, formula } = this.state;
-    if(current === "0" && formula === ""){
-      this.setState({
-        current: "0" + value,
-        formula: current + value
-      })
-    } else if(endsWithDecNumber.test(formula)){
-        this.setState({
-          current: current.includes(".") ? current : current + value,
-          formula: formula
-        })
-    } else if (endsWithOperator.test(formula)){
-        this.setState({
-          current: "0" + value,
-          formula: formula + "0" + value
-        })
-    } else {
-        this.setState({
-          current: current.includes(".") ? current : current + value,
-          formula: !formula.endsWith(".") ? formula + value : formula
-        })
-    }
-  }
-    /*this.setState({
-      current: current.includes(".") ? current : current + value,
-      formula: current.includes(".") ? current : current + value
-    })*/
-
   allClear(){
+    //returns state to initial state
     this.setState({
-      current: 0,
-      prev: 0,
+      current: "0",
+      previous: "",
       formula: "",
       evaluated: false
     })
   }
 
-  getAnswer(){
-    let expression = this.state.formula;
-    while(endsWithOperator.test(expression)) {
-      expression = expression.slice(0, -1);
-    }
-      // eslint-disable-next-line
-      let answer = Math.round(1000000000000 * eval(expression)) / 1000000000000;
-      this.setState({
-        current: answer.toString(),
-        formula: expression + " = " + answer,
-        prev: answer,
-        evaluated: true
-      })
-  }
-
   percentage() {
     let value = this.state.formula;
+    //self-explanatory, but evaluates current formula by giving percentage
     this.setState({
       // eslint-disable-next-line
       current: eval(value/100),
       formula: `${value}% =`,
       evaluated: true
     })
+  }
+
+  getAnswer(){
+    let expression = this.state.formula;
+    //if expression end in an operator, removes the operator prior to evaluating
+    while(endsWithOperator.test(expression)) {
+      expression = expression.slice(0, -1);
+    }
+      //corrects rounding errors of "answer" (borrowed from FCC example)
+      // eslint-disable-next-line
+      let answer = Math.round(1000000000000 * eval(expression)) / 1000000000000;
+      //converts answer to a string, so that "eval" can operate on it...
+      this.setState({
+        current: answer.toString(),
+        formula: expression + " = " + answer,
+        evaluated: true
+      })
   }
 
   render() {
@@ -194,98 +220,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-/* 
- numberClick(e) {
-    const value = e.target.value
-    const { current, formula, evaluated } = this.state;
-      if(evaluated) {
-        this.setState({
-          evaluated: false,
-          prev: current,
-          current: value,
-          formula: value
-        })
-      } else {
-        this.setState({
-          current: startsWithZero.test(current) ? value : current + value,
-          formula: current === 0 && value === 0
-            ? formula === ""
-              ? value
-              : formula
-            : /([^.0-9]0|^0)$/.test(formula)
-              ? formula.slice(0, -1) + value
-              : formula + value
-                });
-      }
-      
-  }
-
-  operatorClick(e) {
-    const value = e.target.value;
-    const { current, formula, evaluated } = this.state;
-    if(evaluated) {
-      this.setState({
-        evaluated: false,
-        prev: current,
-        current: value,
-        formula: current + value
-      })
-    }  else if(endsWithOperator.test(formula)) {
-      this.setState({
-        prev: current,
-        current: value,
-        formula: !negs.test(formula + current) && value === "-" 
-          ? (formula).replace(negs, "-") + value
-          : (formula + current).slice(0, -2) + value
-      })
-    } else {
-        this.setState({
-          prev: current,
-          current: value,
-          formula: formula + value
-      })
-    }
-  }
-
-  decimalClick(e) {
-    const value = e.target.value;
-    const {current, formula } = this.state;
-    if(current.includes("."))
-       {this.setState ({
-          current: current,
-          formula: formula
-      })
-     } else {
-        this.setState ({
-          current: current + value,
-          formula: formula + value
-      })
-      }
-  }
-  
-  allClear() {
-    this.setState({
-      current: 0,
-      prev: 0,
-      formula: ""
-    })
-  }
-  
-  
-
-  getAnswer() {
-    let expression = this.state.formula;
-    while(endsWithOperator.test(expression)) {
-      expression = expression.slice(0, -1);
-    }
-      // eslint-disable-next-line
-      let answer = eval(expression);
-      this.setState({
-        current: answer.toString(),
-        formula: expression + "=" + answer,
-        prev: answer,
-        evaluated: true
-      })
-  }
-*/
